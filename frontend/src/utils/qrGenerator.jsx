f8137a14465b46f7003fa2e87be637597ffc77ff
Sumer Gill <sumergill@Sumer-65-t.local>
2025-11-07 21:44:14 +0530

    fix: ESLint plugin resolution + add utils/qrGenerator.jsx + normalize JSX imports

---
+++ frontend/src/utils/qrGenerator.jsx
@@ -0,0 +1,9 @@
+import QRCode from 'qrcode';
+export async function generateQR(text, opts = {}) {
+  return QRCode.toDataURL(text, {
+    errorCorrectionLevel: 'M',
+    width: 256,
+    margin: 1,
+    ...opts,
+  });
+}
